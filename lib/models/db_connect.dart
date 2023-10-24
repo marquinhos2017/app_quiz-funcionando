fda455d2b337dcc14dd201ea23477ec1b2653c53
Marcos Rodrigues <marcosrodriguescorreiajc@gmail.com>
2023-10-23 21:17:48 -0800

    Corrigindo Link

--- lib/models/db_connect.dart
+++ lib/models/db_connect.dart
@@ -4,7 +4,7 @@ import 'package:app_quiz/models/question_model.dart';
 import 'package:http/http.dart' as http;
 
 class DBconnect{
-  final url = Uri.parse(('https://console.firebase.google.com/u/0/project/appqi-7ca20/database/appqi-7ca20-default-rtdb/data/~2F'));
+  final url = Uri.parse(('https://appqi-7ca20-default-rtdb.firebaseio.com/questions.json'));
 
   Future<void> addQuestion(Question question) async{
     http.post(url,
